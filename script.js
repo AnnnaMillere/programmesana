65388850df6d4a87d2c6d6b289ef2805c7294283
AnnnaMillere <annamillere2008@gmail.com>
2025-03-07 10:29:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,76 @@
-/*
-alert(" Hello world!");
 
-console.log("Hello world!");
-*/
+let a; = parseFloat( prompt("Enter variable a: "));
+let b; = parseFloat( prompt("Enter variable b: "));
+//console.log(typeof(a)); // izvada mainīga a t
+//console.log(typeof(b));
+let result;
+let operators = prompt("Enter operator: ");
 
-/*
-let x = 5;
-let y = 5;
 
-if(x == y)
+
+while(conrinue)
+    
+let turpināt = true;
+
+function getResult()
+
 {
-console.log ("x ir vienāds ar y");
-}
-*/
+    let a = parseFloat(document.getElementById("a_var"));
+    let b = parseFloat(document.getElementById("b_var"));
+    let operators = document.getElementById("operator");
+    console.log(operators);
+    switch(operators)
+{
+    case"+":
+        result = a + b;
+    break;
+
+    case"-":
+        result = a - b;
+    break;
+    
+    case"*":
+        result = a * b;
+    break;
+
+    case"/":
+        result = a / b;
+    break;
+
+    case"^":
+        result = a ^ b;
+    break;
+
+    case"%":
+        result = a % b;
+    break;
+
+    case"!":
+        console.log("!")
+    break;
+
+    case "!":
+        result = 1;
+        for(let i = 1; i < a ; i++)
+        {
+            //result = result * i;
+            result *= i;
+        }
+    break;
+
+    default:
+        console.log("Error: invalid operator");
+    }
+    console.log(Result: ", result);
+    let user = promt("Turpināt ? (Y/N)");
+    if (user == "n"|| user == "N")
+    {
+        turpināt = false;
+    }
 
 
-let number = prompt("Enter number:");
-if(number > 0)
-{
-    console.log(number, "ir pozitīvs");
+    
 }
-if(number < 0)
-{
-    console.log(number, "ir negatīvs");
-}
-if(number == 0)
-{
-    console.log(number, "ir 0");
-}
+
+result = a + b;
+console.log(result ", result");
